f324790eb496c6b1244bb8a26651308210f1abce
navernineteen <42178387+navernineteen@users.noreply.github.com>
2018-08-20 19:29:19 +0700

    Update article.php
    ARGH

--- article.php
+++ article.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
- <link rel="icon" href="assets/image/logo.png">
+ <link rel="icon" href="assets/image/image/icon.png">
   <title>inseed.id - Invest Seaweed in Indonesia</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
@@ -16,11 +16,18 @@
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   <script src="js/jcarousel.responsive.js"></script>
   <script src="js/jquery.jcarousel.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+<script type="text/javascript">
+$(window).load(function() {
+    $("#loading").fadeOut("slow");
+});
+</script>
 </head>
 <body>
+<div id="loading"></div>
 <section id="header">
 <nav class="navbar navbar-expand-lg navbar-light bg-white" id="header">
- <a href="index.php"><img src="assets/image/logo.png" style="  padding: 0 20px 0 20px;
+ <a href="{{URL::to('welcome')}}"><img src="assets/image/logo.png" style="  padding: 0 20px 0 20px;
     height: 75px;
     width: 250px;"></a>  
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -28,25 +35,25 @@
   </button>
  
 
-  <div class="collapse navbar-collapse " id="navbarSupportedContent">
+    <div class="collapse navbar-collapse " id="navbarSupportedContent" >
     <ul class="navbar-nav ml-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="#">HOME <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="{{URL::to('welcome')}}">HOME <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">SEAWEED INVEST</a>
+        <a class="nav-link" href="{{URL::to('invest')}}">SEAWEED INVEST</a>
       </li>
        <li class="nav-item">
-        <a class="nav-link" href="#">SEAWEED MART</a>
+        <a class="nav-link" href="{{URL::to('mart')}}">SEAWEED MART</a>
       </li>
        <li class="nav-item">
-        <a class="nav-link" href="article.php">SEAWEED ARTICLE</a>
+        <a class="nav-link" href="{{URL::to('article')}}">SEAWEED ARTICLE</a>
       </li>
        <li class="nav-item">
-        <a class="nav-link" href="about_us.php">TENTANG KAMI</a>
+        <a class="nav-link" href="{{URL::to('about_us')}}">TENTANG KAMI</a>
       </li>
      <li class="nav-item">
-        <a class="nav-link" href="login.php">MASUK</a>
+        <a class="nav-link" href="{{URL::to('login')}}">MASUK</a>
       </li>
     </ul>
   </div>
@@ -182,7 +189,7 @@
         <div class="container">
           <div class="row">
            <div class="col-xl-4 col-md-12 col-sm-12 col-xs-12 logo_footer">
-               <a href="index.php"><img src="assets/image/logo.png" style="width: 280px; height: auto;"></a>
+               <a href="{{URL::to('welcome')}}"><img src="assets/image/logo.png" style="width: 280px; height: auto;"></a>
                <h6 >Invest Seaweed in Indonesia</h6>
            </div>
         <div class="col-xl-4 col-md-12 col-sm-12 col-xs-12 contact" style="text-align: center; ">
