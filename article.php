dc84f1e685fb90e2563fed153e4e8046dae4eac5
navernineteen <42178387+navernineteen@users.noreply.github.com>
2018-08-20 15:23:21 +0700

    Update article.php

--- article.php
+++ article.php
@@ -27,7 +27,7 @@ $(window).load(function() {
  <div id="loading"></div>
 <section id="header">
 <nav class="navbar navbar-expand-lg navbar-light bg-white" >
- <a href="index.php"><img src="assets/image/logo.png" style="  padding: 0 20px 0 20px;
+ <a href="{{URL::to('welcome')}}"><img src="assets/image/logo.png" style="  padding: 0 20px 0 20px;
     height: 75px;
     width: 250px;"></a>  
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
