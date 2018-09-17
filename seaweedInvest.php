33b68a33921135d5da8099fa055335e9f43b7139
navernineteen <42178387+navernineteen@users.noreply.github.com>
2018-09-17 16:31:09 +0700

    Update seaweedInvest.php

--- seaweedInvest.php
+++ seaweedInvest.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
  <link rel="icon" href="assets/image/icon/icon.png">
-  <title>inseed.id - Seaweed Invest</title>
+  <title>inseed.id - Seaweed Invests</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
   <link rel="stylesheet" href="css/styleCSS.css">
